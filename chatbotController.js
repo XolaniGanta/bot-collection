e006f75dc3be7322eab75c2a1a68f4e5dbeeef49
xolanigAPS <xolanig@aps.holdings>
2022-12-11 21:16:38 +0200

    test

--- chatbotController.js
+++ chatbotController.js
@@ -96,13 +96,40 @@ router.post('/webhook', async (req, res) => {
             let recipientName = incomingMessage.from.name;
             let typeOfMsg = incomingMessage.type; // extract the type of message (some are text, others are images, others are responses to buttons etc...)
             let message_id = incomingMessage.message_id; // extract the message id
-            let incomingTextMessage = incomingMessage.text.body;
-            let filterID = incomingTextMessage.match(/^\d+$/); //if it has numbers 
-            let buttonID = incomingMessage.button_reply.id;
+
+      async  function check_balance(filterID){
+          const users = await user.findAll({
+            where: {
+              identity_number: filterID
+            },
+            limit: 5
+          });
+      
+          if (users && users.length > 0) {
+            // Map the users to their names and balances
+            const forma = users.map(user => `Hello ${user.name} your current balance is R: ${user.balance}`);
+      
+            // Send the message to the recipient
+            await Whatsapp.sendSimpleButtons({
+              message: (`${forma}`),
+              recipientPhone: recipientPhone,
+              listOfButtons: [{
+                title: 'Settle your account',
+                id: 'settle_account'
+              },
+              {
+                title: 'Done',
+                id: 'Done_btn'
+              }]
+            });
+          } 
+        }
             
 
 
            if (typeOfMsg === 'text_message') {
+              let incomingTextMessage = incomingMessage.text.body;
+              let filterID = incomingTextMessage.match(/^\d+$/); //if it has numbers 
               if (filterID === null) {
                 Whatsapp.sendSimpleButtons({
                   message: `Hi ${recipientName} Welcome to BestforU Self Service. Choose what operation do you want to perform`,
@@ -120,6 +147,7 @@ router.post('/webhook', async (req, res) => {
           }
          
      if (typeOfMsg === 'simple_button_message') {
+            let buttonID = incomingMessage.button_reply.id;
             if (buttonID === 'check_balance') {
               await Whatsapp.sendText({
                 message: `For security reasons you required to enter your id number.  `,
@@ -127,38 +155,17 @@ router.post('/webhook', async (req, res) => {
               });
             }
           } else if (typeOfMsg === 'text_message') {
+            let incomingTextMessage = incomingMessage.text.body;
+            let filterID = incomingTextMessage.match(/^\d+$/); //if it has numbers
             if (filterID !== null) {
               // Find all users with the specified identity number
-              const users = await user.findAll({
-                where: {
-                  identity_number: filterID
-                },
-                limit: 5
-              });
-          
-              if (users && users.length > 0) {
-                // Map the users to their names and balances
-                const forma = users.map(user => `Hello ${user.name} your current balance is R: ${user.balance}`);
-          
-                // Send the message to the recipient
-                await Whatsapp.sendSimpleButtons({
-                  message: (`${forma}`),
-                  recipientPhone: recipientPhone,
-                  listOfButtons: [{
-                    title: 'Settle your account',
-                    id: 'settle_account'
-                  },
-                  {
-                    title: 'Done',
-                    id: 'Done_btn'
-                  }]
-                });
-              } 
+              check_balance(filterID)
             }
           } 
         
         
       if(typeOfMsg === 'simple_button_message'){
+        let buttonID = incomingMessage.button_reply.id;
         if (buttonID === 'Done_btn'){
             await Whatsapp.sendText({
               message: `Have a great day!`,
