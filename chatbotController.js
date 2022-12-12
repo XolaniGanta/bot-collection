47a50ed31b8a536909a92e7b76cd26aaacb547ec
xolanigAPS <xolanig@aps.holdings>
2022-12-12 16:09:05 +0200

    test

--- chatbotController.js
+++ chatbotController.js
@@ -146,7 +146,7 @@ router.post('/webhook', async (req, res) => {
                   bodyText: (`${forma}`),
                   recipientPhone: recipientPhone,
                   footerText: 'bestforu self-service',
-                  buttonText:'Select your payment method',
+                  actionTitle:'Select your payment method',
                   listOfSections: [{
                     title: 'Payment Methods',
                     rows: [ {
