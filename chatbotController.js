1475a72e3ba534958966c4b200aa666d99c3db46
xolanigAPS <xolanig@aps.holdings>
2022-12-14 14:24:14 +0200

    her

--- chatbotController.js
+++ chatbotController.js
@@ -3,7 +3,7 @@
 const router = require('express').Router();
 const WhatsappCloudAPI = require('whatsappcloudapi_wrapper');
 const {Sequelize, DataTypes} = require("sequelize");
-//const isValidDate = require('is-valid-date');
+const isValidDate = require('is-valid-date');
 
 
 const Whatsapp = new WhatsappCloudAPI({
@@ -127,10 +127,10 @@ router.post('/webhook', async (req, res) => {
           } else if (typeOfMsg === 'text_message') {
             let incomingTextMessage = incomingMessage.text.body;
             let filterID = incomingTextMessage.match(/^\d+$/); //detect numbers
-            let count = incomingTextMessage.length;
-          //  let dob = incomingTextMessage.substring(0,6);
+           // let count = incomingTextMessage.length;
+          let dob = incomingTextMessage.substring(0,6);
           //  && count === 13 && isValidDate(dob)
-            if (filterID!== null  && count === 13 ) {
+            if (filterID!== null  && isValidDate(dob) ) {
               // Find all users with the specified identity number
               const users = await clientinfo.findAll({
                 where: {
