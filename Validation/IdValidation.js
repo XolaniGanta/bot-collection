310a7255859d370f5a7c09955988b6cfd8d0256a
xolanigAPS <xolanig@aps.holdings>
2022-12-14 13:15:21 +0200

    her

--- Validation/IdValidation.js
+++ Validation/IdValidation.js
@@ -1,30 +1,30 @@
-const { isValidDate } = require('is-valid-date');
+// const { isValidDate } = require('is-valid-date');
 
-function isValid(idNumber) {
-  // Check that the number is 13 digits long
-  if (idNumber.length !== 13) {
-    return false;
-  }
+// function isValid(idNumber) {
+//   // Check that the number is 13 digits long
+//   if (idNumber.length !== 13) {
+//     return false;
+//   }
 
-  // Check that the first 6 digits represent the person's date of birth in the format YYMMDD
-  const dateOfBirth = idNumber.substring(0, 6);
-  if (!isValidDate(dateOfBirth)) {
-    return false;
-  }
+//   // Check that the first 6 digits represent the person's date of birth in the format YYMMDD
+//   const dateOfBirth = idNumber.substring(0, 6);
+//   if (!isValidDate(dateOfBirth)) {
+//     return false;
+//   }
 
-  // Perform the modulus 11 test to validate the entire number
-  const weightingFactors = [2, 4, 8, 5, 10, 9];
-  let total = 0;
-  for (let i = 0; i < 6; i++) {
-    total += weightingFactors[i] * parseInt(idNumber[i], 10);
-  }
-  if (total % 11 !== 0) {
-    return false;
-  }
-  // If all checks pass, the number is valid
-  return true;
-}
+//   // Perform the modulus 11 test to validate the entire number
+//   const weightingFactors = [2, 4, 8, 5, 10, 9];
+//   let total = 0;
+//   for (let i = 0; i < 6; i++) {
+//     total += weightingFactors[i] * parseInt(idNumber[i], 10);
+//   }
+//   if (total % 11 !== 0) {
+//     return false;
+//   }
+//   // If all checks pass, the number is valid
+//   return true;
+// }
 
-module.exports = {
-  isValid: isValid
-}
+// module.exports = {
+//   isValid: isValid
+// }
