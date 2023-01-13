a8deabd395c2a0a953ae86dfa3bee0da124cd906
xolanigAPS <xolanig@aps.holdings>
2023-01-13 14:44:30 +0200

    maker

--- chatbotController.js
+++ chatbotController.js
@@ -5,6 +5,7 @@ const WhatsappCloudAPI = require('whatsappcloudapi_wrapper');
 const {Sequelize, DataTypes} = require("sequelize");
 const { WebClient } = require('@slack/web-api');
 const emoji = require('node-emoji');
+const bolds = require('node-strings');
 
 
 //Enter slack token to gain Access to Slack API
@@ -97,7 +98,7 @@ router.post('/webhook', async (req, res) => {
         if (data?.isMessage) {
             let incomingMessage = data.message;
             let recipientPhone = incomingMessage.from.phone; // extract the phone number of sender
-            let recipientName = incomingMessage.from.name;
+           // let recipientName = incomingMessage.from.name;
             let typeOfMsg = incomingMessage.type; // extract the type of message (some are text, others are images, others are responses to buttons etc...)
             let message_id = incomingMessage.message_id; // extract the message id
         
@@ -107,7 +108,7 @@ router.post('/webhook', async (req, res) => {
               let filterID = incomingTextMessage.match(/^[a-zA-Z]+$/); //if its only letters
               if (filterID !== null) {
                 Whatsapp.sendSimpleButtons({
-                  message: `Hi `+emoji.get(':wave:')+ ` Welcome to Money Pay, powered by Bestforu - the safe, easy way to pay and check balance on your account.\n\nLets get started...\n\nChoose an option below and click a button for what you'd like to do\n\nShortcut`+emoji.get(':bulb:')+`: If you need help reply with # to chat with an agent`,
+                  message: `Hi `+emoji.get(':wave:')+ ` Welcome to Money Pay, powered by Bestforu - the safe, easy way to pay and check balance on your account.\n\nLets get started...`+bolds.bold('\n\nChoose an option below and click a button for what you like to do')+`\n\nShortcut`+emoji.get(':bulb:')+`: If you need help reply with # to chat with an agent`,
                   recipientPhone: recipientPhone,
                   listOfButtons: [{
                       title: 'Pay my Account',
@@ -118,6 +119,7 @@ router.post('/webhook', async (req, res) => {
                   }
                 ]
                 });
+               
               }
           }
           if (typeOfMsg === 'text_message') {
